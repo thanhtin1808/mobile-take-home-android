4a96d53dc93cd939fd940808af6dbb80a15b617d
TinGiang <thanhtingiang@gmail.com>
2024-09-17 13:06:11 +0700

    added cached image config

--- app/src/main/java/com/android/takehome/TakeHomeApp.kt
+++ app/src/main/java/com/android/takehome/TakeHomeApp.kt
@@ -1,11 +1,17 @@
 package com.android.takehome
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class TakeHomeApp : Application() {
+class TakeHomeApp : Application(), ImageLoaderFactory {
 
     override fun onCreate() {
         super.onCreate()
@@ -13,4 +19,21 @@ class TakeHomeApp : Application() {
             Timber.plant(Timber.DebugTree())
         }
     }
+    override fun newImageLoader(): ImageLoader = ImageLoader(this).newBuilder()
+        .memoryCachePolicy(CachePolicy.ENABLED)
+        .memoryCache {
+            MemoryCache.Builder(this)
+                .maxSizePercent(0.1)
+                .strongReferencesEnabled(true)
+                .build()
+        }
+        .diskCachePolicy(CachePolicy.ENABLED)
+        .diskCache {
+            DiskCache.Builder()
+                .maxSizePercent(0.03)
+                .directory(cacheDir)
+                .build()
+        }
+        .logger(DebugLogger())
+        .build()
 }
